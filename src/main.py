344b24e57b3a089b85a3924837c661dbe0568071
Vazgen005 <norvardyanvazgen@gmail.com>
2022-11-05 21:41:14 +0300

    one more rewrite
    Co-Authored-By: zertmark <47692978+zertmark@users.noreply.github.com>

--- src/main.py
+++ src/main.py
@@ -1,110 +1,125 @@
+from rich.progress import Progress
+from rich.console import Console
 import yandex_music
-import logging
 import asyncio
 import json
 import sys
 import os
 import re
 
+console = Console()
 
-logging.getLogger().setLevel(logging.DEBUG)
+class Client(yandex_music.Client):
+    def __init__(self, token:str = "") -> None:
+        yandex_music.Client.notice_displayed = True
+        super().__init__(token)
+        if self.isValidToken():
+            Token.writeTokenToConfig(token)
+            return
+        console.log("Token is invalid\nExiting...")
+        sys.exit(1)
+             
+    def isValidToken(self) -> bool:
+        try:
+            self._request.get(f"{self.base_url}/account/status")
+        except yandex_music.exceptions.UnauthorizedError or yandex_music.exceptions.TimedOutError:
+            return False
+        return True
 
-class Client:
-    def login(self):
-        client = None
-        token = ''
-        
+class Token:
+    @staticmethod
+    def getToken() -> str:
         if os.path.exists('config.json'):
             with open('config.json', 'r') as f:
                 try:
                     token = json.load(f)['token']
-                except json.decoder.JSONDecodeError:
-                    token = input('Enter token: ')
-                except KeyError:
+                except json.decoder.JSONDecodeError or KeyError:
                     token = input('Enter token: ')
         else:
             token = input('Enter token: ')
-
-        try:
-            client = yandex_music.Client(token).init()
-        except yandex_music.exceptions.UnauthorizedError:
-            print('Invalid token')
-            sys.exit(1)
-
+        return token
+        
+    @staticmethod
+    def writeTokenToConfig(token:str = "") -> None:
         with open('config.json', 'w') as f:
             f.write(json.dumps({'token': token}))
-        return client
 
 class StringOperations:
-    def fix_string(self, string):
+    @staticmethod
+    def fixString(string):
         for symbol in ['\\', '/', ':', '*', '?', '"', '<', '>', '|']:
             string = string.replace(symbol, '')
         return string
 
-    def fix_track_info(self, track_info: dict):
+    @staticmethod
+    def fixTrackInfo(track_info: dict):
         if track_info['playlist_title'] == '' or track_info['playlist_title'] == None:
             track_info['playlist_title'] = 'unnamed'
         for key in track_info:
             if isinstance(track_info[key], str):
-                track_info[key] = self.fix_string(track_info[key])
+                track_info[key] = StringOperations.fixString(track_info[key])
         return track_info
+    
+    @staticmethod
+    def joinArtists(artists: list):
+        return ', '.join(list(map(lambda x: StringOperations.fixString(x['name']), artists)))
+
+    @staticmethod
+    def getMusicDirectory():
+        return f'{os.getcwd()}\\music\\'
 
 class Downloads:
-    SO = StringOperations()
-    def playlist_parser(self, url):
-        id_ = 0
-        owner = ''
+    def parsePlaylist(self, url):
         regex = re.search('(https://)?(www\.)?music\.yandex\.ru/users/([A-Za-z0-9-_]+)/playlists/([0-9]+)?', url, re.IGNORECASE)
         if regex:
             return client.users_playlists(regex.group(4), regex.group(3))
         else:
-            print('invalid url')
+            console.log('Invalid url was given')
             sys.exit(1)
-        
-    def get_music_directory(self):
-        return f'{os.getcwd()}\\music\\'
-    
-    def join_artists(self, artists: list):
-        return ', '.join(list(map(lambda x: self.SO.fix_string(x['name']), artists)))
 
-    def get_path_for_playlist(self, track_info: dict):
-        path: str = f'{self.get_music_directory()}{track_info["playlist_title"]}'
+    def getPathForPlaylist(self, track_info: dict):
+        path: str = f'{StringOperations.getMusicDirectory()}{track_info["playlist_title"]}'
         if not os.path.exists(path):
             os.makedirs(path)
         return path
         
-    def get_path_for_tack(self, track_info: dict):
-        return f"{self.get_path_for_playlist(track_info)}\\{self.join_artists(track_info['track_artists'])} - {track_info['track_title']}.mp3"
+    def getPathForTrack(self, track_info: dict):
+        return f"{self.getPathForPlaylist(track_info)}\\{StringOperations.joinArtists(track_info['track_artists'])} - {track_info['track_title']}.mp3"
 
-    async def download_tracks(self, playlist):
+    async def downloadTracks(self, playlist):
+        self.progressBar = Progress(console=console)
+        self.progressBar.start()
+        self.taskId = self.progressBar.add_task("[green]Downloading music...", total=len(playlist.tracks)) 
         await asyncio.gather(*[self.task(track, playlist) for track in playlist.tracks]) 
-
+        self.progressBar.stop()
+        
     async def task(self, track, playlist):
         track_info = {
         'track_artists': track['track']['artists'],
         'track_title': track['track']['title'],
         'playlist_title': playlist['title'],
         }
+        exceptions = {
+            yandex_music.exceptions.TimedOutError : "Timed out. Retrying...",
+            yandex_music.exceptions.NetworkError: "Network error. Retrying...",
+        }
 
-        track_info = self.SO.fix_track_info(track_info=track_info)
+        track_info = StringOperations.fixTrackInfo(track_info=track_info)
 
-        if not os.path.exists(self.get_path_for_tack(track_info)):
+        if not os.path.exists(self.getPathForTrack(track_info)):
             try:
-                track['track'].download(self.get_path_for_tack(track_info))
-                print(f"Downloaded: {os.path.basename(self.get_path_for_tack(track_info)).replace('.mp3', '')}")
-            except yandex_music.exceptions.TimedOutError:
-                print('Timed out. Retrying...')
+                track['track'].download(self.getPathForTrack(track_info))
+                console.log(f"{os.path.basename(self.getPathForTrack(track_info)).replace('.mp3', '')}")
+            except Exception as exception:
+                console.log("\n", exceptions[exception] if exception in exceptions.keys() else f"Unknow error:\n{exception}")
                 await asyncio.sleep(1)
                 await self.task(track, playlist)
-            except yandex_music.exceptions.NetworkError:
-                print('Network error. Retrying...')
-                await asyncio.sleep(1)
-                await self.task(track, playlist)
-        else:
-            print(f"Already downloaded: {os.path.basename(self.get_path_for_tack(track_info)).replace('.mp3', '')}")
+        else:        
+            console.log(f"Already downloaded: {os.path.basename(self.getPathForTrack(track_info)).replace('.mp3', '')}")
+        self.progressBar.update(self.taskId, advance = 1)
 
 if __name__ == "__main__":
-    client = Client().login()
+    client = Client(Token.getToken()).init()
     Downloads_ = Downloads()
-    asyncio.run(Downloads_.download_tracks(Downloads_.playlist_parser(url=input("Please, enter a url to a playlist: "))))
-    print('Done!')
+    asyncio.run(Downloads_.downloadTracks(Downloads_.parsePlaylist(url=console.input("Please, enter a url to a playlist: "))))
+    console.log('Done!')
